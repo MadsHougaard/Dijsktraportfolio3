4bfcf00c05a80cb5df1459cfd1febc9a242daee5
simonhumtek <sisikr@ruc.dk>
2019-11-19 18:35:22 +0000

    Små ændringer til kommentarer og lignende

--- DijkstraPortfolio3test/DijkstraPortfolio3/src/Graph.java
+++ DijkstraPortfolio3test/DijkstraPortfolio3/src/Graph.java
@@ -4,13 +4,15 @@ import javafx.util.Pair;
 public class Graph {
     private ArrayList<Vertex> Vertices = new ArrayList<>();
 
-    public Vertex addvertex(String id) {
+    public Vertex addvertex(String id) 
+    {
         Vertex newvertex = new Vertex(id);
         Vertices.add(newvertex);
         return newvertex;
     }
 
-    public void addvertex(Vertex v) {
+    public void addvertex(Vertex v) 
+    {
         Vertices.add(v);
     }
     public Vertex getvertex(String s)
@@ -23,18 +25,19 @@ public class Graph {
         return null;
     }
 
-    public void newedge(Vertex from, Vertex to, int dist, int tim) {
-        Edge newedge=new Edge(from,to);
+    public void newedge(Vertex from, Vertex to, int dist, int tim) 
+    {
+        Edge newedge = new Edge(from,to);
         newedge.distance=dist;
         newedge.time=tim;
     }
 
     public Pair<Integer, Map<Vertex,Vertex> > ShortestDistance(Vertex source, Vertex zink)
     {
-        Map<Vertex,Vertex> PredecessorMap= new HashMap<>();
-        Map<Vertex,Integer> DistanceMap=new HashMap<>();
-        Map<Vertex,Integer> UnvisitedMap=new HashMap<>();
-        // initialize arrays
+        Map<Vertex,Vertex> PredecessorMap = new HashMap<>();
+        Map<Vertex,Integer> DistanceMap = new HashMap<>();
+        Map<Vertex,Integer> UnvisitedMap = new HashMap<>();
+        // Initialiserer arrays
         for(Vertex v: Vertices)
         {
             DistanceMap.put(v,1000);
@@ -42,13 +45,14 @@ public class Graph {
             UnvisitedMap.put(v, 1000); // Giver os punkter som vi har besøgt
         }
 
-        DistanceMap.put(source,0); //lav startpunkt
-        UnvisitedMap.put(source,0); // lav startpunkt på anden liste. Skal være nul fordi det er startpunkt dvs. ingen længde derhen.
-        //implement Dijkstra
+        DistanceMap.put(source,0); // lav startpunkt
+        UnvisitedMap.put(source,0); // lav startpunkt på anden liste. Skal være nul fordi det er startpunkt dvs. ingen længde derhen
+        // Implement Dijkstra
 
         // Gennemgå alle ubesøgte punkter og udregn distancen til punktet naboer
         // Så længe der stadig er ubesøgte punkter do stuff
-        while (UnvisitedMap.size() > 0) {
+        while (UnvisitedMap.size() > 0) 
+        {
             Vertex currentVertex = getmin(UnvisitedMap); // Udvælg det ubesøgte punkt der har den korteste distance p.t.
 
             int currentDistance = UnvisitedMap.get(currentVertex); // Finder Node, tager distance værdi og gemmer i currentDistance
@@ -56,22 +60,27 @@ public class Graph {
             UnvisitedMap.remove(currentVertex); // Fjern currentVertex fordi den ny antages som besøgt. Vi vønsker ikke at støde på den igen når vi gennemgår listen UnvisitedMap
 
             // Løb igennem alle Edges der er forbundet med currentVertex
-            for(Edge edge : currentVertex.getOutEdges()) {
+            for(Edge edge : currentVertex.getOutEdges()) 
+            {
                 Vertex neighbourVertex = edge.getTovertex(); // Finder punkt
 
-                int neighbourDistance = currentDistance + edge.distance; // lav vores distance til nabo ved at finde vores
+                int neighbourDistance = currentDistance + edge.distance; // Lav vores distance til nabo ved at finde den totale distance dertil
 
-                if(neighbourDistance < DistanceMap.get(neighbourVertex) ) {
+                if(neighbourDistance < DistanceMap.get(neighbourVertex) ) 
+                {
                     DistanceMap.put(neighbourVertex, neighbourDistance);
                     PredecessorMap.put(neighbourVertex, currentVertex);
-                } //hvis nabo distance er mindre end distance
-                if (UnvisitedMap.get(neighbourVertex) != null) {
+                } 
+                // Sætter nabovertex distanceværdi lig med den totale distance dertil
+                if (UnvisitedMap.get(neighbourVertex) != null) 
+                {
                     UnvisitedMap.put(neighbourVertex, neighbourDistance);
                 }
             }
         }
 
-        for (Map.Entry<Vertex, Integer> entry : DistanceMap.entrySet()) {
+        for (Map.Entry<Vertex, Integer> entry : DistanceMap.entrySet()) 
+        {
             System.out.println("Node: "+entry.getKey().Name+"  -  Distance: " + entry.getValue());
         }
 
@@ -80,13 +89,15 @@ public class Graph {
         return (new Pair<Integer,Map<Vertex,Vertex>> (DistanceMap.get(zink), PredecessorMap));
     }
 
-    // Returner Vertex med korteste distance på en vilkårlig liste (Map)
+    // Returnerer vertex med korteste distance på en vilkårlig liste (Map)
     public Vertex getmin(Map<Vertex,Integer> qmap){
         int shortestDistance = 9999;
         Vertex shortestVertex = null;
-        for (Map.Entry<Vertex, Integer> entry : qmap.entrySet()) {
+        for (Map.Entry<Vertex, Integer> entry : qmap.entrySet()) 
+        {
 
-            if (entry.getValue() < shortestDistance) {
+            if (entry.getValue() < shortestDistance) 
+            {
                 shortestVertex = entry.getKey();
                 shortestDistance = entry.getValue();
             }
@@ -97,7 +108,7 @@ public class Graph {
 
 
 
-class Vertex{
+class Vertex {
     public String Name;
     public ArrayList<Edge> OutEdges = new ArrayList<>();
     public  Vertex(String id){
@@ -106,10 +117,10 @@ class Vertex{
     public void addOutEdge(Edge outedge){
         OutEdges.add(outedge);
     }
-    public ArrayList<Edge> getOutEdges(){return OutEdges;}
+    public ArrayList<Edge> getOutEdges() {return OutEdges;}
 }
 
-class Edge{
+class Edge {
     private Vertex fromvertex;
     private Vertex tovertex;
     public int distance=0;
@@ -118,12 +129,12 @@ class Edge{
     public Vertex getTovertex() {
         return tovertex;
     }
-    //directional boolean
-    public Edge(Vertex from, Vertex to){
+    // Directional boolean
+    public Edge(Vertex from, Vertex to) {
         fromvertex=from;
         tovertex=to;
         fromvertex.addOutEdge(this);
-        //If not directional
+        // Hvis ikke directional
         tovertex.addOutEdge(this);
     }
 }
